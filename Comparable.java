1165398116d93a55b19e3315ba65c0535cf75b14
OsamaNasser1 <144779057+OsamaNasser1@users.noreply.github.com>
2023-09-22 02:24:17 +0300

    Updated Comparable.java

--- Comparable.java
+++ Comparable.java
@@ -1,6 +1,10 @@
 
-public interface Comparable<T> {
+public interface Comparable{
 	public void insert(String contactName, String phoneNumber, String email, String address, String birthday, String notes);
 	public void remove(String k);
-	
+	public void SearchByName(String val);
+	public void SearchByPhoneNumber(String val);
+	public void SearchByEmailAdress(String val);
+	public void SearchByAdress(String val);
+	public void SearchByBirthday(String val);
 }
