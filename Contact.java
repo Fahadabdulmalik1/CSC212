888a8206c3781469a753636d126eb5d1bc69d4e8
FahadAliii <145114434+FahadAliii@users.noreply.github.com>
2023-09-23 21:36:13 +0300

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,12 +1,12 @@
 
 public class Contact {
-	public String contactName;
-	public String phoneNumber;
-	public String email;
-	public String address;
-	public String birthday;
-	public String notes;
-	public Contact next;
+	private String contactName;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String birthday;
+	private String notes;
+	private Contact next;
 	public Contact(String contactName, String phoneNumber, String email, String address, String birthday, String notes) {
 		this.contactName = contactName;
 		this.phoneNumber = phoneNumber;
@@ -16,6 +16,49 @@ public class Contact {
 		this.notes = notes;
 		next=null;
 	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public Contact getNext() {
+		return next;
+	}
+	public void setNext(Contact next) {
+		this.next = next;
+	}
 	
+
 
 }
